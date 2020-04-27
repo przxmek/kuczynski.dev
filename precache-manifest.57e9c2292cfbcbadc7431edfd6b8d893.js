da5ddd31b5a3f90f9e10299625cab5401d940c35
Przemysław Kuczyński <przemek@kuczynski.dev>
2020-04-28 01:16:42 +0200

    Updates

---
+++ precache-manifest.57e9c2292cfbcbadc7431edfd6b8d893.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c596d4fb0f688b71afb9e6330283323",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99f1576e8222177f3ba9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "21de43a383ab145fdea3",
+    "url": "/static/js/2.b81b39a1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b81b39a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f1576e8222177f3ba9",
+    "url": "/static/js/main.838021de.chunk.js"
+  },
+  {
+    "revision": "0100127600c0f331ffbd",
+    "url": "/static/js/runtime-main.2c80d949.js"
+  },
+  {
+    "revision": "a0237e52a0ec1f74bcc8440feacb3007",
+    "url": "/static/media/avatar.a0237e52.jpg"
+  }
+]);
