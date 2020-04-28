2e60856f2b8395c4098ee9d7c8f83632d0afa155
Przemysław Kuczyński <przemek@kuczynski.dev>
2020-04-28 19:42:14 +0200

    SignIn view /sign-in

--- src/views/SignIn/SignInView.tsx
+++ src/views/SignIn/SignInView.tsx
@@ -1,8 +1,113 @@
 import React from 'react';
+import {
+  makeStyles,
+  Theme,
+  createStyles,
+  Container,
+  CssBaseline,
+  Avatar,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Grid,
+  Box,
+  Link
+} from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  // const useStyles = makeStyles((theme: Theme) => createStyles({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const SignInView = () => {
-  return (<h1>Sign in page</h1>)
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <FontAwesomeIcon icon={faLock} />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+          </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+            </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link to="/forgot-password" variant="body2" component={RouterLink}>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link to='/sign-up' variant="body2" component={RouterLink}>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        {/* <Copyright /> */}
+      </Box>
+    </Container>
+  );
 }
 
 export default SignInView;
